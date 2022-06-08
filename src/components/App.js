8d3f6940c9a7c94e46caba4e88f6d481f38aaf4d
AmanB-2003 <90694309+AmanB-2003@users.noreply.github.com>
2022-06-08 20:58:13 +0530

    first-commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+//<img src={logo} className="App-logo" alt="logo" />
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <label>Your City </label>
+      <input type="text" ></input>
+    </div>
+  );
+}
+
+export default App;
