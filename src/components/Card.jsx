e267f74912a3119bf187330df71bf30f04267c9a
AmanB-2003 <90694309+AmanB-2003@users.noreply.github.com>
2024-05-06 20:32:39 +0530

    alt image

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ export default function Card(props){
     return <div className="col-sm-2">
     <div className="card border-0 " style={{ width: "100%"}}>
         <h3>{props.date}</h3>
-        <img src={props.image}> alt="card-img"</img>
+        <img src={props.image} alt="card-img"></img>
         <p>Humidity</p>
         <p>{props.humidity} %</p>
     </div>
